2194478bfad9e0653cae83b0f3e0cf4b186f5f57
hetvi-7span <hetvi@7span.com>
2023-05-17 12:28:02 +0530

    project structure

---
+++ friendlist/src/main/java/com/bytro/friendlist/FriendListManagementApp.java
@@ -0,0 +1,11 @@
+package com.bytro.friendlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendListManagementApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FriendListManagementApp.class);
+    }
+}
