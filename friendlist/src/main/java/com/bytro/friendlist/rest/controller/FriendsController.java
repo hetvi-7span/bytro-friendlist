717634b57f91891fa458c157d31be43bb41347e4
hetvi-7span <hetvi@7span.com>
2023-05-22 18:25:42 +0530

    swagger summary

--- friendlist/src/main/java/com/bytro/friendlist/rest/controller/FriendsController.java
+++ friendlist/src/main/java/com/bytro/friendlist/rest/controller/FriendsController.java
@@ -2,6 +2,7 @@ package com.bytro.friendlist.rest.controller;
 
 import com.bytro.friendlist.handler.FriendsHandler;
 import com.bytro.friendlist.shared.record.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class FriendsController {
     FriendsController(final FriendsHandler friendsHandler) {
         this.friendsHandler = friendsHandler;
     }
-
+    @Operation(summary = "Unfriend a friend")
     @PostMapping("/unfriend/{userId}/{friendId}")
     public BaseResponse<Void> unfriend(
             @PathVariable Integer userId, @PathVariable Integer friendId) {
