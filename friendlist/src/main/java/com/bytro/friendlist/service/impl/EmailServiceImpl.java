2d31d4c40bc6769610cbf03f6d085f131036b9c5
hetvi-7span <hetvi@7span.com>
2023-05-22 13:13:56 +0530

    log implementation

--- friendlist/src/main/java/com/bytro/friendlist/service/impl/EmailServiceImpl.java
+++ friendlist/src/main/java/com/bytro/friendlist/service/impl/EmailServiceImpl.java
@@ -2,6 +2,8 @@ package com.bytro.friendlist.service.impl;
 
 import com.bytro.friendlist.service.EmailService;
 import com.bytro.friendlist.shared.record.response.EmailDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Service;
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender javaMailSender;
+
+    private static final Logger LOG = LoggerFactory.getLogger(EmailServiceImpl.class);
 
     EmailServiceImpl(final JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
@@ -26,6 +30,7 @@ public class EmailServiceImpl implements EmailService {
             mailMessage.setText(details.msgBody());
             mailMessage.setSubject(details.subject());
             javaMailSender.send(mailMessage);
+            LOG.info("Email sent successfully.");
         } catch (Exception e) {
             e.printStackTrace();
         }
