3ac97a9cb68984a520587231efc63b53c6d74513
anup-7span <anup@7span.com>
2023-05-23 19:38:44 +0530

    PA_PHPM-55 implement flyway

---
+++ friendlist/src/main/resources/db/migration/V1__Create_Table.sql
@@ -0,0 +1,8 @@
+CREATE
+    TABLE
+        friendlist_management.test(
+            id serial NOT NULL,
+            name VARCHAR(255),
+            uid VARCHAR(255),
+            PRIMARY KEY(id)
+        );
