ce61c795e9bc877f273676c5cc3f2f3a87d3ea5d
anup-7span <anup@7span.com>
2023-05-25 19:19:44 +0530

    Revised PHPM-55

---
+++ friendlist/src/main/resources/db/migration/V1__PHPM_TABLE.sql
@@ -0,0 +1,24 @@
+  CREATE TABLE friend_request (
+      id serial4 NOT NULL,
+      created_at timestamp(6) NULL,
+      created_by varchar(255) NULL,
+      updated_at timestamp(6) NULL,
+      updated_by varchar(255) NULL,
+      message varchar(255) NULL,
+      receiver_id int4 NOT NULL,
+      sender_id int4 NOT NULL,
+      status varchar(255) NULL,
+      CONSTRAINT friend_request_pkey PRIMARY KEY (id)
+  );
+
+  CREATE TABLE friends (
+      friend_id int4 NOT NULL,
+      user_id int4 NOT NULL,
+      created_at timestamp(6) NULL,
+      created_by varchar(255) NULL,
+      updated_at timestamp(6) NULL,
+      updated_by varchar(255) NULL,
+      blocked_by int4 NOT NULL,
+      is_blocked bool NOT NULL,
+      CONSTRAINT friends_pkey PRIMARY KEY (friend_id, user_id)
+  );
